bdfdf8ad6e319f3e2f1aa49b71c8d8b2f0488094
smoochy User <smoochy@ubuntu.ubuntu-domain>
2010-12-04 23:31:10 -0800

    Updates to product model and templates

--- shop/models.py
+++ shop/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
-    sku = models.CharField(max_length=20)
+    asin = models.CharField(max_length=15)
     price = models.DecimalField(max_digits=9, decimal_places=2)
     publisher = models.CharField(max_length=50)
     description = models.TextField()
