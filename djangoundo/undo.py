518ff2fde8792fa0698d7d4a949d69386ff90ed6
smoochy User <smoochy@ubuntu.ubuntu-domain>
2010-12-06 22:54:10 -0800

    Undo stack methods

---
+++ djangoundo/undo.py
@@ -0,0 +1,31 @@
+import pickle
+
+try:
+    from django.conf.settings import DJANGO_UNDO_SESSION_KEY
+except ImportError:
+    DJANGO_UNDO_SESSION_KEY = 'undo'
+    
+
+def stow(request, object):
+    if DJANGO_UNDO_SESSION_KEY not in request.session:
+        request.session[DJANGO_UNDO_SESSION_KEY] = {}
+    pickled_object = pickle.dumps(object)
+    
+    undo_dict = request.session[DJANGO_UNDO_SESSION_KEY]
+    object_id = str(object.id)
+    undo_dict.setdefault(object_id, pickled_object)
+    request.session[DJANGO_UNDO_SESSION_KEY] = undo_dict
+    
+    object.delete()
+    
+
+def restore(request, object_id):
+    undo_dict = request.session[DJANGO_UNDO_SESSION_KEY]
+    pickled_object = undo_dict[object_id]
+    object = pickle.loads(pickled_object)
+    object.save()
+    if object.pk != None:
+        del undo_dict[object_id]
+        request.session[DJANGO_UNDO_SESSION_KEY] = undo_dict
+    return object
+        
